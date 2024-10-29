d9990efdb70bc7280628781b7a1123cd81e008b7
PedroYorlano <pedro.yorlano@gmail.com>
2024-10-29 19:23:36 -0300

    definicion de metodos create, findById, findAll, update y delete en PruebaService

--- agencia/src/main/java/com/tpi/agencia/service/PruebaService.java
+++ agencia/src/main/java/com/tpi/agencia/service/PruebaService.java
@@ -5,6 +5,8 @@ import com.tpi.agencia.repositories.PruebaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PruebaService {
     private final PruebaRepository repository;
@@ -17,4 +19,24 @@ public class PruebaService {
     public PruebaEntity create(PruebaEntity prueba) {
         return repository.save(prueba);
     }
+
+    public Optional<PruebaEntity> findById(Integer id) {
+        return repository.findById(id);
+    }
+
+    public Iterable<PruebaEntity> findAll() {
+        return repository.findAll();
+    }
+
+    public PruebaEntity update(Integer id, PruebaEntity prueba) {
+        if (repository.existsById(id)) {
+            return repository.save(prueba);
+        }
+        throw new IllegalArgumentException("No se encontro la prueba con ID: " + id);
+    }
+
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
+
 }
