4948248a75f7bc8c70c1c8af733d259e697d50d4
Tomas Vergara <tominacho@hotmail.com>
2024-10-29 21:27:06 -0300

    Se actualizo para usar SQLITE

---
+++ gateway/src/main/java/com/tpi/gateway/config/GWConfig.java
@@ -0,0 +1,97 @@
+package com.tpi.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GWConfig {
+
+    /*
+       Este bean toma como parametros los values de las URL a los otros dos microservicios que se encuentran
+       como variables en el application.properties.
+    */
+    @Bean
+    public RouteLocator configurarRutas(RouteLocatorBuilder builder,
+                                        @Value("${tpi-agencia-api-gw.microservicio-agencia}") String uriAgencia,
+                                        @Value("${tpi-agencia-api-gw.microservicio-notificaciones}") String uriNotificaciones) {
+
+        // Registra las rutas que usa para redireccioanr las peticiones a los microservicios especificos.
+        return builder.routes()
+                // Ruteo al Microservicio de Agencia
+                .route(p -> p.path("/api/v1/agencia/**").uri(uriAgencia))
+                // Ruteo al Microservicio de Notificaciones
+                .route(p -> p.path("/api/v1/notificaciones/**").uri(uriNotificaciones))
+                .build();
+    }
+
+    /* Aca se definen las politicas de acceso que requieren los diferentes endopoints.
+        1. Unicamente un empleado puede acceder a crear pruebas y mandar notificaciones.
+        2. Unicamente un usuario asociado a un vehiculo puede enviar posiciones.
+        3. Unicamnete un administrador puede ver los datos de los reportes
+     */
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges
+
+                        // Solo empleados pueden crear pruebas y enviar notificaciones
+                        .pathMatchers("/api/v1/agencia/pruebas/new")
+                        .hasRole("EMPLEADO")
+
+                        .pathMatchers("/api/v1/notificaciones/**")
+                        .hasRole("EMPLEADO")
+
+                        // Solo usuarios asociados a vehículos pueden enviar posiciones
+                        .pathMatchers("/api/v1/agencia/pruebas/posicion")
+                        .hasRole("USUARIO")
+
+                        // Solo administradores pueden ver reportes
+                        .pathMatchers("/api/v1/agencia/reportes/**")
+                        .hasRole("ADMINISTRADOR")
+
+                        // Rutas de pruebas accesibles para cualquiera
+                        .pathMatchers("/api/v1/agencia/pruebas/**")
+                        .permitAll()
+
+                        // Cualquier otra petición específica de agencia accesible para todos
+                        .pathMatchers("/api/v1/agencia/**")
+                        .permitAll()
+
+                        // Para cualquier peticion hace falta estar autenticado.
+                        .anyExchange()
+                        .authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+        // Se especifica el nombre del claim a analizar
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+        // Se agrega este prefijo en la conversión por una convención de Spring
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                new ReactiveJwtGrantedAuthoritiesConverterAdapter(grantedAuthoritiesConverter));
+        return jwtAuthenticationConverter;
+    }
+
+
+}
