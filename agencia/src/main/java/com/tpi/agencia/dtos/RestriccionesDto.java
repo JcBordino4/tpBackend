15923b20af96066fb5b2d2891ee134929255135e
Tomas Vergara <tominacho@hotmail.com>
2024-11-04 10:30:56 -0300

    Se agrego un servicio que busca las restricciones del servidor externo de la catedra y las cachea.

---
+++ agencia/src/main/java/com/tpi/agencia/dtos/RestriccionesDto.java
@@ -0,0 +1,25 @@
+package com.tpi.agencia.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class RestriccionesDto {
+    private Coordenadas coordenadasAgencia;
+    private double radioAdmitidoKm;
+    private List<ZonaRestringida> zonasRestringidas;
+
+    @Data
+    public static class Coordenadas {
+        private double lat;
+        private double lon;
+    }
+
+    @Data
+    public static class ZonaRestringida {
+        private Coordenadas noroeste;
+        private Coordenadas sureste;
+    }
+}
