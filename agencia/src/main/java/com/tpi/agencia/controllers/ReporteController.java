83a7a6c2d97e6b18527aced32190b1d3ff95b4a2
JcBordino4 <95008@sistemas.frc.utn.edu.ar>
2024-11-12 11:22:14 -0300

    Eliminacion de Slf4j en ReporteController

--- agencia/src/main/java/com/tpi/agencia/controllers/ReporteController.java
+++ agencia/src/main/java/com/tpi/agencia/controllers/ReporteController.java
@@ -8,7 +8,6 @@ import com.tpi.agencia.dtos.report.responses.IncidentesXEmpleado;
 import com.tpi.agencia.dtos.report.responses.PruebasResponse;
 import com.tpi.agencia.service.ExternalApisService;
 import com.tpi.agencia.service.ReporteService;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
