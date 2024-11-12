f4f056c6c4fbde1d681b4bb58bd0e8a3677b6e8c
PedroYorlano <pedro.yorlano@gmail.com>
2024-11-12 12:42:13 -0300

    comentarios descriptivos de las consultas

--- agencia/src/main/java/com/tpi/agencia/repositories/PruebaRepository.java
+++ agencia/src/main/java/com/tpi/agencia/repositories/PruebaRepository.java
@@ -7,16 +7,23 @@ import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface PruebaRepository extends CrudRepository<PruebaEntity, Integer> {
+    /*
+        La siguiente query hace una consulta sobre si existe alguna prueba
+        perteneciente a un vehiculo(buscado segun su id) cuya fecha y hora de
+        fin sean nulos, es decir, este en curso
+     */
     @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END " +
             "FROM PruebaEntity p WHERE p.vehiculo.id = ?1 AND p.fechaHoraFin IS NULL")
     boolean existsByVehiculoIdAndFechaHoraFinIsNull(@Param("idVehiculo") Integer idVehiculo);
 
     List<PruebaEntity> findByFechaHoraFinIsNull();
 
-
+    /*
+        La siguiente query hace una consulta que busca una prueba
+        perteneciente a un vehiculo(buscado segun su id) y haya sido notificada
+    */
     @Query("SELECT p FROM PruebaEntity p WHERE p.vehiculo.id = :vehiculoId " +
             "AND (p.fechaHoraFin IS NULL AND :fechaNotificacion BETWEEN p.fechaHoraInicio AND CURRENT_TIMESTAMP " +
             "OR :fechaNotificacion BETWEEN p.fechaHoraInicio AND p.fechaHoraFin)")
@@ -24,6 +31,10 @@ public interface PruebaRepository extends CrudRepository<PruebaEntity, Integer> 
             @Param("vehiculoId") Integer vehiculoId,
             @Param("fechaNotificacion") LocalDateTime fechaNotificacion);
 
+    /*
+        La siguiente query es similar a la anterior, pero busca las pruebas
+        para un empleado en particular, buscado por su id
+     */
     @Query("SELECT p FROM PruebaEntity p WHERE p.vehiculo.id = :vehiculoId " +
             "AND p.empleado.legajo = :idEmpleado " +
             "AND (p.fechaHoraFin IS NULL AND :fechaNotificacion BETWEEN p.fechaHoraInicio AND CURRENT_TIMESTAMP " +
